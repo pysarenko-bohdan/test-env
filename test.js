0bb383c24b45b7a5c4dd2e4627101af96f0b9118
pysarenko-bohdan <bohdan.pisarenko@codefresh.io>
2023-12-09 19:58:18 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,4 +2,5 @@ const fullStr = process.env.TEST_ENV;
 const printLog = (str, type) => console.log(`${str} - ${type}`;
 printLog(fullStr, 'full');
 printLog(fullStr.substring(0,1), 'trimmed');
-if (!fullStr) printLog('Empty', 'string');
+if (!fullStr) printLog('Empty', 'string')
+
